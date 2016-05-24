1b21471aff01b5469e204c90d14e0a0beb4f891b
StlMaris123 <stellamarisnjage@gmail.com>
2016-05-24 12:10:21 +0300

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-  	render text :"Its getting better!"
+  	render text: "Its getting better!"
   end
 end
